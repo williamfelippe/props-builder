dc947e208f00318c380bf1fc7832ae9f46134106
William Felippe <wfelippesilva@gmail.com>
2024-02-06 13:40:30 +0000

    refactor: updating example

--- example/src/counter/counter.builder.ts
+++ example/src/counter/counter.builder.ts
@@ -1,13 +1,13 @@
 import { CounterProps } from "./";
 import { propsBuilder } from '../../../src/index'
 
-import { randNumber } from '@ngneat/falso'
 import { vi } from 'vitest'
+import { faker } from '@faker-js/faker'
 
 export const counterBuilder = propsBuilder<CounterProps>(
   'CounterBuilder', 
-  {
-    count: randNumber(),
+  () => ({
+    count: faker.number.int(),
     changeCount: vi.fn()
-  }
+  })
 )
--- example/src/counter/counter.test.tsx
+++ example/src/counter/counter.test.tsx
@@ -9,21 +9,21 @@ import { counterBuilder } from './counter.builder';
 import user from '@testing-library/user-event'
 
 test('should show count', () => {
-  const props = counterBuilder()
+  const props = counterBuilder.buildOne()
   render(<Counter {...props} />)
 
   expect(screen.getByRole('button', { name: `count is ${props.count}` }))
 })
 
 test('should show count (fixed value)', () => {
-  const props = counterBuilder({ count: 2 })
+  const props = counterBuilder.buildOne({ count: 2 })
   render(<Counter {...props} />)
 
   expect(screen.getByRole('button', { name: 'count is 2' }))
 })
 
 test('should execute callback when button is clicked', async () => {
-  const props = counterBuilder()
+  const props = counterBuilder.buildOne()
   render(<Counter {...props} />)
 
   const button = screen.getByRole('button')
